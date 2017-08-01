54e8c0b20f7dfb6e38547c359cf67421ac921fed
Yarrney <alex.arney@gmail.com>
2017-08-01 22:06:07 +1000

    added dockerfile and modified content

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sed -i "s/define('AUTH_KEY', '')/define('AUTH_KEY', '${AUTH_KEY}')/g" /data/mkhugh/wp-config.php
-sed -i "s/define('SECURE_AUTH_KEY', '')/define('SECURE_AUTH_KEY', '${SECURE_AUTH_KEY}')/g" /data/mkhugh/wp-config.php
-sed -i "s/define('LOGGED_IN_KEY', '')/define('LOGGED_IN_KEY', '${LOGGED_IN_KEY}')/g" /data/mkhugh/wp-config.php
-sed -i "s/define('NONCE_KEY', '')/define('NONCE_KEY', '${NONCE_KEY}')/g" /data/mkhugh/wp-config.php
-sed -i "s/define('AUTH_SALT', '')/define('AUTH_SALT', '${AUTH_SALT}')/g" /data/mkhugh/wp-config.php
-sed -i "s/define('SECURE_AUTH_SALT', '')/define('SECURE_AUTH_SALT', '${SECURE_AUTH_SALT}')/g" /data/mkhugh/wp-config.php
-sed -i "s/define('LOGGED_IN_SALT', '')/define('LOGGED_IN_SALT', '${LOGGED_IN_SALT}')/g" /data/mkhugh/wp-config.php
-sed -i "s/define('NONCE_SALT', '')/define('NONCE_SALT', '${NONCE_SALT}')/g" /data/mkhugh/wp-config.php
+echo "define('AUTH_KEY', '${AUTH_KEY}');" >> /data/mkhugh/wp-config.php
+echo "define('SECURE_AUTH_KEY', '${SECURE_AUTH_KEY}');" >> /data/mkhugh/wp-config.php
+echo "define('LOGGED_IN_KEY', '${LOGGED_IN_KEY}');" >> /data/mkhugh/wp-config.php
+echo "define('NONCE_KEY', '${NONCE_KEY}');" >> /data/mkhugh/wp-config.php
+echo "define('AUTH_SALT', '${AUTH_SALT}');" >> /data/mkhugh/wp-config.php
+echo "define('SECURE_AUTH_SALT', '${SECURE_AUTH_SALT}');" >> /data/mkhugh/wp-config.php
+echo "define('LOGGED_IN_SALT', '${LOGGED_IN_SALT}');" >> /data/mkhugh/wp-config.php
+echo "define('NONCE_SALT', '${NONCE_SALT}');" >> /data/mkhugh/wp-config.php
 
 sed -i "s/define('DB_NAME', '')/define('DB_NAME', '${DB_NAME}')/g" /data/mkhugh/wp-config.php
 sed -i "s/define('DB_USER', '')/define('DB_USER', '${DB_USER}')/g" /data/mkhugh/wp-config.php
